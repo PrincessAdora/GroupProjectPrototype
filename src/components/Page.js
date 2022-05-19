0a582e6e50d0e5bfbe7af5215fc68c3fefe6f191
PrincessAdora <89851664+PrincessAdora@users.noreply.github.com>
2022-05-18 21:37:01 -0400

    changed offset of icons and buttons to only the first page; finish setting up buttons and address inability to click buttons issue. next step is graph and search which is up to mel ray to make

--- src/components/Page.js
+++ src/components/Page.js
@@ -8,12 +8,15 @@ import Brooklyn from "../images/Bridge.png";
 function Page({ offset, color }) {
   return (
     <>
+      {/* Set offset to a specific number if you only want that layer to show on one page,
+    set offset to offset if you want that layer to show on every page */}
       {/* add reusable layers here as Page component*/}
 
+      {/* SOMETHING IN BACKGROUND PUT AN INVISIBLE OVERLAY OVER THE WEBPAGE, PREVENTING YOU FROM BEING ABLE TO CLICK ANYTHING */}
       {/*Background Parallax layer -- behind all other layers */}
-      <ParallaxLayer offset={offset} speed={1}>
+      {/* <ParallaxLayer offset={offset} speed={1}>
         <div className="background"></div>
-      </ParallaxLayer>
+      </ParallaxLayer> */}
 
       {/*Color Parallax Layer -- over background layer */}
       <ParallaxLayer offset={offset} speed={2.1}>
@@ -37,51 +40,51 @@ function Page({ offset, color }) {
         <Navbar />
       </ParallaxLayer>
 
-      <ParallaxLayer offset={offset} speed={3.5} style={{ marginTop: "100px" }}>
-        <div className="icon1">
-          <img src={Bronx} width="100" height="100" alt="" />
-        </div>
+      <ParallaxLayer offset={0} speed={1.7} style={{ marginTop: "100px" }}>
+        <img src={Bronx} width="100" height="100" alt="" className="icon1" />
       </ParallaxLayer>
 
-      <ParallaxLayer offset={offset} speed={3.5} style={{ marginTop: "100px" }}>
-        <div className="icon2">
-          <img src={Manhattan} width="100" height="100" alt="" />
-        </div>
+      <ParallaxLayer offset={0} speed={1.85} style={{ marginTop: "100px" }}>
+        <img
+          src={Manhattan}
+          width="100"
+          height="100"
+          alt=""
+          className="icon2"
+        />
       </ParallaxLayer>
 
-      <ParallaxLayer offset={offset} speed={3.5} style={{ marginTop: "100px" }}>
-        <div className="icon3">
-          <img src={Brooklyn} width="100" height="100" alt="" />
-        </div>
+      <ParallaxLayer offset={0} speed={2} style={{ marginTop: "100px" }}>
+        <img src={Brooklyn} width="100" height="100" alt="" className="icon3" />
       </ParallaxLayer>
 
       <ParallaxLayer
-        offset={offset}
-        speed={3.5}
+        offset={0}
+        speed={1.2}
         style={{ marginTop: "200px", marginLeft: "-150px" }}
       >
-        <div className="button1">
+        <button className="button">
           {/* Bronx Button */}
-          <button>BRONX</button>
-        </div>
+          <div>BRONX</div>
+        </button>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={offset} speed={3.5} style={{ marginTop: "200px" }}>
-        <div className="button2">
+      <ParallaxLayer offset={0} speed={1.35} style={{ marginTop: "200px" }}>
+        <button className="button">
           {/* Manhattan Button */}
-          <button>MANHATTAN</button>
-        </div>
+          <div>MANHATTAN</div>
+        </button>
       </ParallaxLayer>
 
       <ParallaxLayer
-        offset={offset}
-        speed={3.5}
+        offset={0}
+        speed={1.4}
         style={{ marginTop: "200px", marginLeft: "150px" }}
       >
-        <div className="button3">
+        <button className="button">
           {/* Brooklyn Button */}
-          <button>BROOKLYN</button>
-        </div>
+          <div>BROOKLYN</div>
+        </button>
       </ParallaxLayer>
     </>
   );
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,10 +4,11 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function Scroll() {
   return (
-    <Parallax pages={3} className="container" horizontal>
+    <Parallax pages={4} className="container" horizontal>
       <Page offset={0} color="light" />
       <Page offset={1} color="mid" />
       <Page offset={2} color="dark" />
+      <Page offset={3} color="darker" />
       {/* <Page offset={2} color="purple" />
       <Page offset={3} color="pink" /> */}
     </Parallax>
